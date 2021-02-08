37dc7898341dab87dd6ba389b42c3196465fccaf
AmoDinho <u14284783@tuks.co.za>
2021-02-08 22:17:40 +0200

    created the index

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,12 @@
 import { hello } from "./query";
-import { IResolvers } from "graphql-tools/dist/Interfaces"
+import { createPurchase } from "./mutation";
+import { IResolvers } from "graphql-tools/dist/Interfaces";
 export const resolvers: IResolvers = {
-    Query: {
-        hello: (root: any, args: any, context: any) => hello(args, context)
-    }
-}
+  Query: {
+    hello: (root: any, args: any, context: any) => hello(args, context),
+  },
+  Mutation: {
+    createPurchase: (root: any, args: object, context: object) =>
+      createPurchaseargs(args, context),
+  },
+};
