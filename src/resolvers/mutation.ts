4020bb7590cd0022e27398022697906db387f84f
AmoDinho <u14284783@tuks.co.za>
2021-02-18 22:02:22 +0200

    imported utils func

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,13 +1,12 @@
 import * as dynamoDB from "../../libs/dynamoDB";
-
+import * as Utils from "../../libs/utils";
 export const createPurchase = async (args: any, context: object) => {
   const PurchaseInput: object = {
     ...args.purchase,
   };
   const params = {
     TableName: process.env.PurchasesDB,
-    Item: { ID:,
-      ...PurchaseInput },
+    Item: { ID: Utils.uuidv4(), ...PurchaseInput },
   };
 
   try {
